eecea729d7ee6cfd6164e894863f9e2f284a4de2
Morgan Holmes <morgan.holmes.ps@gmail.com>
2024-06-12 19:06:59 +0200

    working v1.0

--- buckets.js
+++ buckets.js
@@ -1,7 +1,8 @@
 const BUCKETS__PLUGIN_NAME = "buckets";
-const BUCKETS__DELAY = 300;
+const BUCKETS__DELAY = 500;
 const BUCKETS__AMOUNT_UNIT = "mL";
 const BUCKETS__BASE_LABEL = "Total O-Count";
+const BUCKETS__ACTIVE_PAGES = ["/stats"];
 const BUCKETS__NEW_LABEL = `${BUCKETS__BASE_LABEL} / Juice Amount`;
 const BUCKETS__AMOUNTS = {
     low: 2,
@@ -14,13 +15,23 @@ const BUCKETS__DEFAULT_CONFIGURATION = {
 
 setTimeout(async () => {
     const userConfiguration = await csLib.getConfiguration(BUCKETS__PLUGIN_NAME, {});
-    const {meanAmount} = {
+    const {meanAmount: configuredMeanAmount} = {
         ...BUCKETS__DEFAULT_CONFIGURATION,
         ...userConfiguration
-    }
+    };
+    const meanAmount = configuredMeanAmount !== '' ? configuredMeanAmount : BUCKETS__DEFAULT_CONFIGURATION.meanAmount;
 
     const addBucketsNode = () => {
         setTimeout(() => {
+
+            if(!BUCKETS__ACTIVE_PAGES.includes(window.location.pathname)){
+                return;
+            }
+
+            if(meanAmount !== "low" && meanAmount !== "regular" && meanAmount !== "high"){
+                console.error(`Invalid value for meanAmount. Valid values are 'low', 'regular' or 'high', but found value '${meanAmount}'. Fix this in Settings > Plugins > Buckets`);
+                return;
+            }
 
             const statsElements = Array.prototype.slice.call(document.getElementsByClassName("stats-element"));
 
